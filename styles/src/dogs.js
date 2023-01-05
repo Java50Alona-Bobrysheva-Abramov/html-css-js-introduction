c007f21fd2eda741dee1a25afecba78f0cfbb3e7
Alona <evmoiseenko.gmail.com>
2023-01-06 00:03:40 +0200

    hw #6 applying a function and creating "click"

--- styles/src/dogs.js
+++ styles/src/dogs.js
@@ -1,15 +1,38 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
+const mainElement = document.querySelector(".main-class")
+const HIDDEN = "hidden";
+const hideButtonElement = document.querySelector("#hide-button");
+
+
+
+function showDetails (){
+    mainElement.classList.remove(HIDDEN);
+}
+
+
+function hideDetails (){
+    mainElement.classList.add(HIDDEN);
+}
+
+
 function setDetails(anchor) {
-
-
     const dataImage = anchor.getAttribute("data-details-image");
    /* detailsImage.src = "images/3a.jpg";
     detailsTitle.innerHTML = "I love baby";*/
 
     detailsImage.src = dataImage;
-    detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    detailsTitle.innerHTML = anchor.getAttribute("data-details-title"); 
+
+    showDetails();
+
+    const color = anchor.getAttribute("data-text-color");
+    if (color) {
+        detailsTitle.style.color = color;
+    } else {
+        detailsTitle.style.color = '';
+    }
 }
 
 for (let i = 0; i < anchorElements.length; i++) {
@@ -17,4 +40,5 @@ for (let i = 0; i < anchorElements.length; i++) {
         setDetails(anchorElements[i]);
     } )
 }  
+  hideButtonElement.addEventListener("click",hideDetails);
       
