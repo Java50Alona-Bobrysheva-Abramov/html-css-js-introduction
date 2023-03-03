4f6e5336eee1551a0fbdc06a5d7532ff03dd5815
Alona Abramov <evmoiseenko@gmail.com>
2023-03-03 21:24:38 +0200

    HW 19 dynamic adding field operator

--- app.js
+++ app.js
@@ -1,59 +1,61 @@
-const employee1 = {id: 123, name: "Vasya", birthYear: 2000, 
-salary: 15000, address: {city: "Lod", country: "Israel"}};
-const employee2 = {id: 123, name: "Vasya", birthYear: 2000, 
-salary: 15000, address: {city: "Lod", country: "Israel"}};
-console.log(`employee1==employee2 is ${employee1 == employee2}`)
-const employee3 = employee1;
-console.log(`employee3==employee1 is ${employee3 == employee1}`)
-employee3.salary = 20000;
-console.log(`employee1 salary = ${employee1.salary} `)
-function createEmployee(id, name, birthYear, salary, city, country) {
-    return {id, name, birthYear, salary, address: {city, country}}
-}  
-const employees = [
-    createEmployee(123, "Vasya", 2000, 15000, "Lod", "Israel"),
-    createEmployee(124, "David", 1975, 15500, "Tel Aviv", "Israel"),
-    createEmployee(125, "Sara", 1985, 20000, "New York", "USA"),
-    createEmployee(126, "Abraham", 1990, 13000, "London", "UK"),
-    createEmployee(127, "Moshe", 2000, 15000, "Rehovot", "Israel"),
-    createEmployee(128, "Goga", 1993, 10000, "Tbilisi", "Gorgia"),
-    createEmployee(129, "Sasha", 2000, 25000, "Ramat Gan", "Israel"),
-    createEmployee(130, "Victor", 2003, 10000, "Arad", "Israel")
-]
-//const index = employees.indexOf(createEmployee(126, "Abraham", 1990, 13000, "London", "UK"))
-const index = employees.findIndex(function(empl) {
-    return empl.id === 126;
+arrayOccurrences.sort(stringComp);
+arrayOccurrences.forEach(entry => console.log(`${entry[0]} -> ${entry[1]}`));
+}
+function getStringOccurrences(strings) {
+function getStringsOccurrences(strings) {
+const res = {};
+strings.forEach(str => {
+    if(!res[str]) {
+@@ -113,14 +113,27 @@ function stringComp(entry1, entry2) {
+}
+return res;
+}
+displayStringOccurrences(strings);
+//displayStringOccurrences(strings);
+//HW #19
+function getMostPopulatedCountry(employees) {
+//returns country with most amount of employees
+const countries = getCountriesSortedByOccurrences(employees);
+return countries[0];
+}
+
+function getMostPupulatedCountries(employees, counter) {
+//returns a given number (conter) of countries with most amount of employees
+const countries = getCountriesSortedByOccurrences(employees);
+return countries.slice(0, counter);
+
+}
+function getCountriesSortedByOccurrences(employees) {
+const countries = employees.map(e => e.address.country);
+const countriesOccurrences = getStringsOccurrences(countries);
+const arrayOccurrences = Object.entries(countriesOccurrences);
+arrayOccurrences.sort(stringComp);
+return arrayOccurrences.map(entry => entry[0]);
+
+}
+function isAnagram(word, anagram) {
+//TODO 
+@@ -130,6 +143,25 @@ function isAnagram(word, anagram) {
+//hello anagram examples: elolh, olleh, ohell, lehol
+//exampls non-anagrams: eloll (no h), ollehh(different length),
+// olaeh ("a" doesn't exist in word), oleh(different length)
+let res = false;
+if (word.length === anagram.length) {
+    const letters = Array.from(word);
+const lettersOccurrences = getStringsOccurrences(letters);
+const anagramLetters = Array.from(anagram);
+res = anagramLetters.every(letter => {
+    let res = false;
+    if (lettersOccurrences[letter]) {
+        res = true;
+        lettersOccurrences[letter]--;
+        if(lettersOccurrences[letter] == 0) {
+            delete lettersOccurrences[letter];
+        }
+    }
+    return res;
 })
-const employee = employees.find(function(empl) {
-    return empl.id === 126;
-})
-
-
-
-
-
-//HW #18
-function getEmployee(employees, id) {
-    //TODO
-    //returns reference to an Employee object with a given id value
 }
-function getEmployeesBySalary(employees, salaryFrom, salaryTo) {
-    //TODO
-    //returns array of Employee objects that have salary in [salaryFrom, salaryTo]
-}
-function getEmployeesByCity(employees, city) {
-    //TODO
-    //returns array of Employee objects from a given city
-}
-function getEmployeeNames(employees) {
-    //TODO
-    //returns array of all Employee names
-}
-function sortEmployeesByAge(employees) {
-    //TODO
-    //returns array of Employee objects sorted by age in ascending order
-}
-function computeSalaryBudget(employees) {
-    //TODO
-    //computes and returns total salary for all Employee objects
+return res;
+
 }
